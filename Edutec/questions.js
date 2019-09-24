ed4eef72590d1fbaf20b2bec49311ac0b0c471e0
jfnhdn <51129191+jfnhdn@users.noreply.github.com>
2019-09-24 01:49:21 -0300

    Add files via upload

---
+++ Edutec/questions.js
@@ -0,0 +1,56 @@
+var questions = [
+    
+
+    [". Assinale a alternativa que não representa uma função das coordenadas geográficas: ",
+
+        "a) localizar os hemisférios de uma determinada área", "b) estabelecer noções relativas de distância", "c) empreender a proporção entre uma área territorial e sua representação cartográfica", "d) encontrar a exata posição de um determinado ponto no mapa","e) localizar graficos",
+        "", "", "", "", "","",
+        "3"
+    ],
+
+    [" Para a localização de um ponto sobre a superfície terrestre são utilizadas as coordenadas geográficas: latitude e longitude. Se, por exemplo, uma cidade possui alta latitude, pode-se afirmar que ela apresenta uma localização próxima: ",
+    
+    "a) ao antimeridiano de Greenwich", "b) ao meridiano de Greenwich ", "c) a um dos polos terrestres", "d) à linha do Equador","e) a linha de Greenwitch",
+    "", "", "", "", "","",
+    "3"
+   ],
+
+   ["Paralelos e meridianos são linhas imaginárias que permitem localizar qualquer ponto na superfície terrestre. Essas linhas determinam dois tipos de coordenada: latitude e longitude. O mapa abaixo apresenta cinco pontos, localizados em coordenadas diferentes e representados pelas letras A, B, C, D e E. A partir da figura abaixo e com base no sistema de coordenadas, é correto afirmar que:  ",
+    
+   "a) o ponto A está localizado a 40° de latitude oeste e a 80° de longitude norte;", "b) o ponto C está localizado a 60° de latitude norte e a 40° de longitude leste; ", "c) o ponto B está localizado a 20° de latitude sul e a 20° de longitude oeste;", "d)o ponto D está localizado a 20° de latitude norte e a 20° de longitude oeste","e) o ponto E está localizado a 40° de latitude leste e a 100° de longitude leste.",
+   "img/tres.PNG", "", "", "", "","",
+   "2"
+  ],
+
+  [" No espaço aéreo brasileiro, uma aeronave se desloca, em linha reta, de Palmas, no Tocantins, para Brasília, no Distrito Federal. De acordo com os pontos cardeais, essa aeronave descreve uma trajetória no sentido  ",
+    
+  "a) sul – norte ", "b) leste – oeste  ", "c) norte – sul", "d) nordeste – sudoeste","e) sudoeste – nordeste",
+  "", "", "", "", "","",
+  ""
+ ],
+
+ [" ",
+    
+ "", " ", "", "","",
+ "", "", "", "", "","",
+ ""
+],
+
+["  ",
+    
+"", " ", "", "","",
+"", "", "", "", "","",
+""
+],
+
+["  ",
+    
+"", " ", "", "","",
+"", "", "", "", "","",
+""
+],
+    
+
+
+]
+          
